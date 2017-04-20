712f1e12765cb482de930220b2ab5fbcfdbea49e
Thomas Miles <thomas.miles.tgm@gmail.com>
2017-04-21 09:31:38 +1000

    First commit

---
+++ src/jebediah/skynet.java
@@ -0,0 +1,33 @@
+package jebediah;
+
+import aiproj.slider.Move;
+import aiproj.slider.SliderPlayer;
+
+/**
+ * Created by Tom Miles (626263) & George Juliff (624946) on 21/04/2017.
+ *
+ *
+ * AI agent for playing (and winning) the slider game
+ */
+public class skynet implements SliderPlayer {
+
+
+
+    public void init(int dimension, String board, char player) {
+
+    }
+
+
+    public void update(Move move) {
+
+    }
+
+
+    public Move move() {
+
+        Move move;
+
+
+        return move;
+    }
+}
